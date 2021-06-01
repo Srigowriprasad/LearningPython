77eb0a4f5c9102fb2fb9c877a52d9193789632c0
Srigowriprasad <srigowriprasad@gmail.com>
2021-06-01 17:28:44 +0530

    Initial commit

---
+++ Demo/Func.py
@@ -0,0 +1,17 @@
+x = 20
+def trialFunc(x):
+    for a in range(100):
+        print(a)
+    b=0
+    while b<=5:
+        b += 1
+        print(b)
+if x>5:
+    x=10
+
+
+def func():
+    print(x)
+print(len('programmer'))
+print(trialFunc(x))
+print("We are \"Vikings\" of the north")
---
+++ Demo/Helloworld.py
@@ -0,0 +1,48 @@
+# Code to print - Hello world
+
+print("HelloWorld!!!")
+
+a = 5
+b= 10
+if 5>3:
+    print("In if")
+x= a//b
+a=5
+A=50
+a=b=c='sringsofvalues as'
+d = """ Oranges ' As of now '
+A multiline
+strings
+' in the python'
+"""
+e = ''' a single quoted
+multi line
+strings
+'''
+print(a)
+print(A)
+print(e)
+f = "str,str1,asd, cd"
+print(f[0])
+print(type(x))
+print(d)
+print(b)
+x,y,z = "a","b","c"
+print(x)
+print(y)
+for i in f:
+    print(i)
+
+print(len(f))
+print("str1,s" in f)
+print(f[2:5])
+print(f[-5:])
+print(f.strip())
+print(f.replace("st","rt"))
+print("str"+"\n adder")
+
+if a==b and a>b:
+    print("")
+
+elif a==b:
+    print(a)
